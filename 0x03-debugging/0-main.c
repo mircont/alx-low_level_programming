1e9471166cebd19bb12f4e32328949fa87ffb57c
mircont <gouffia05@gmail.com>
2023-09-13 04:00:32 -0700

    Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - test that the function gives the correct output
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
+
