3f1cb4edf73b80718dd7912a752cc833aacac29a
mircont <gouffia05@gmail.com>
2023-09-28 10:16:35 -0700

    They say we only use a fraction of our brain's true potential

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,55 @@
+#include "main.h"
+/**
+ * is_palindrome - if a string is a palindrome
+ * @s: string
+ * Return: On success 1
+ * other ways 0 returned
+ */
+int is_palindrome(char *s)
+{
+	return (mircont_palindrome(s, _strlen_recursion(s) - 1));
+}
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: pointer to string
+ * Return: Always len
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == 0)
+		return (0);
+
+	else
+	{
+		if (*s == '\0')
+		{
+			return (0);
+		}
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
+
+/**
+ * mircont_palindrome - if a string is a palindrome
+ * @s: string
+ * @i: difference
+ * Return: On success 1
+ * other ways 0 returned
+ */
+int mircont_palindrome(char *s, int i)
+{
+	if (*s == *(s + i))
+	{
+		if (i <= 0)
+			return (1);
+
+		else
+		{
+			return (mircont_palindrome((s + 1), i - 2));
+		}
+	}
+	else
+		return (0);
+}
+
