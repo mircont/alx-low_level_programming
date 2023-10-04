55cef7fe5d1ecaac91c278823c9638ae2c5352b9
mircont <gouffia05@gmail.com>
2023-10-04 22:12:22 +0100

    He who is not courageous enough to take risks will accomplish nothing in life-7

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,39 +3,49 @@
 #include <stdio.h>
 
 /**
- *  * str_concat - a function that concatenates two strings.
- *   *
- *    * @s1: input to string 1
- *     * @s2: input to string 2
+ *  * str_concat - Function that concatenates two strings.
+ *   * 
+ *    * @s1: the first string
+ *     * @s2: the second string
  *      *
- *       * Return: NULL on faliure
- *       */
+ *       * Return: char pointer, pointer to the new string or null if failed
+ *        */
 
-char *str_concat(char *s1, char *s2) {
+char *str_concat(char *s1, char *s2)
+{
+		char *array;
+			unsigned int size, i, s1_size = 0, s2_size = 0;
 
-	  if (s1 == NULL) {
-		      s1 = "";
-		        }
-	    if (s2 == NULL) {
-		        s2 = "";
-			  }
+				if (s1 == NULL)
+							s1 = "";
 
+					if (s2 == NULL)
+								s2 = "";
 
-	      int len = strlen(s1) + strlen(s2) + 1;
+						for (i = 0; s1[s1_size] != '\0'; s1_size++)
+									continue;
 
+							for (; s2[s2_size] != '\0'; s2_size++)
+										continue;
 
-	        char *s = malloc(len);
-		  if (s == NULL) {
-			      return NULL;
-			        }
+								size = s1_size + s2_size;
 
+									array = malloc(sizeof(char) * size + 1); /* +1 for the null char  */
 
-		    strcpy(s, s1);
+										if (!array)
+													return (NULL);
 
+											for (i = 0; i < size; i++)
+													{
+																if (i < s1_size)
+																			{
+																							array[i] = s1[i];
+																									}
+																		else
+																						array[i] = s2[i - s1_size];
+																			}
 
-		      strcpy(s + strlen(s1), s2);
-
-		        
-		        return s;
+												array[size] = '\0';
+													return (array);
 }
 
